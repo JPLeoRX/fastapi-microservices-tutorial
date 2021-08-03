861ebb952ef3d847002bb4574ec1d292dbceea8c
leo <leo.ertuna@gmail.com>
2021-08-03 03:32:17 +0300

    Initialize utils

---
+++ shared-utils/library_utils/__init__.py
@@ -0,0 +1,7 @@
+from .abstract_converter import AbstractConverter
+from .utils_id import UtilsId
+
+__all__ = [
+    'AbstractConverter',
+    'UtilsId'
+]
---
+++ shared-utils/library_utils/abstract_converter.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class AbstractConverter:
+    def map_do_to_mo(self, item_do):
+        pass
+
+    def map_mo_to_do(self, item_mo):
+        pass
+
+    def map_add_to_mo(self, item_add):
+        pass
+
+    def map_update_to_mo(self, item_update):
+        pass
+
+    def map_list_do_to_list_mo(self, item_do_list: List) -> List:
+        return [self.map_do_to_mo(item_do) for item_do in item_do_list]
