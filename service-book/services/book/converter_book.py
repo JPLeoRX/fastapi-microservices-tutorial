7cd260fec31bd73f4b57a0a9eee45b6495ddf1b7
leo <leo.ertuna@gmail.com>
2021-08-06 04:04:32 +0300

    Real book service & converter

--- service-book/services/book/converter_book.py
+++ service-book/services/book/converter_book.py
@@ -1,23 +1,56 @@
 from .book_do import BookDO
 from library_message_protocol import Book, BookAdd, BookUpdate
-from library_utils import AbstractConverter
+from library_utils import AbstractConverter, UtilsId, UtilsTime
 from injectable import injectable, autowired, Autowired
 
 
 @injectable
 class ConverterBook(AbstractConverter):
     @autowired
-    def __init__(self):
-        pass
+    def __init__(self, utils_id: Autowired(UtilsId), utils_time: Autowired(UtilsTime)):
+        self.utils_id = utils_id
+        self.utils_time = utils_time
 
     def map_do_to_mo(self, item_do: BookDO) -> Book:
-        pass
+        return Book(
+            id=item_do.id,
+            title=item_do.title,
+            author_id=item_do.author_id,
+            publish_year=item_do.publish_year,
+            description=item_do.description,
+            created_timestamp=item_do.created_timestamp,
+            updated_timestamp=item_do.updated_timestamp
+        )
 
     def map_mo_to_do(self, item_mo: Book) -> BookDO:
-        pass
+        return BookDO(
+            id=item_mo.id,
+            title=item_mo.title,
+            author_id=item_mo.author_id,
+            publish_year=item_mo.publish_year,
+            description=item_mo.description,
+            created_timestamp=item_mo.created_timestamp,
+            updated_timestamp=item_mo.updated_timestamp
+        )
 
     def map_add_to_mo(self, item_add: BookAdd) -> Book:
-        pass
+        return Book(
+            id=self.utils_id.generate_uuid(),
+            title=item_add.title,
+            author_id=item_add.author_id,
+            publish_year=item_add.publish_year,
+            description=item_add.description,
+            created_timestamp=self.utils_time.get_current_timestamp_ms(),
+            updated_timestamp=self.utils_time.get_current_timestamp_ms()
+        )
 
-    def map_update_to_mo(self, item_update: BookUpdate) -> Book:
-        pass
+    def map_update_to_mo(self, item_old: Book, item_update: BookUpdate) -> Book:
+        return Book(
+            id=item_old.id,
+            title=item_update.title,
+            author_id=item_update.author_id,
+            publish_year=item_update.publish_year,
+            description=item_update.description,
+            created_timestamp=item_old.created_timestamp,
+            updated_timestamp=self.utils_time.get_current_timestamp_ms()
+        )
---
+++ service-book/services/book/service_book.py
@@ -0,0 +1,43 @@
+from typing import List
+from sqlalchemy.orm import Session
+from library_message_protocol import Book, BookAdd, BookUpdate
+from library_utils import AbstractServiceDatabase
+from .book_do import BookDO
+from .converter_book import ConverterBook
+from injectable import injectable, autowired, Autowired
+
+
+@injectable
+class ServiceBook(AbstractServiceDatabase):
+    @autowired
+    def __init__(self, converter: Autowired(ConverterBook)):
+        self.converter = converter
+
+    def get_all(self, db: Session, offset: int = 0, limit: int = 100) -> List[Book]:
+        items_do = db.query(BookDO).order_by(BookDO.title).offset(offset).limit(limit).all()
+        return self.converter.map_list_do_to_list_mo(items_do)
+
+    def get_by_id(self, db: Session, id: str) -> Book:
+        item_do = db.query(BookDO).filter(BookDO.id == id).first()
+        return self.converter.map_do_to_mo(item_do)
+
+    def add(self, db: Session, item_add: BookAdd) -> Book:
+        item_mo = self.converter.map_add_to_mo(item_add)
+        item_do = self.converter.map_mo_to_do(item_mo)
+        db.add(item_do)
+        db.commit()
+        return item_mo
+
+    def update(self, db: Session, item_update: BookUpdate) -> Book:
+        item_mo = self.get_by_id(db, item_update.id)
+        item_mo = self.converter.map_update_to_mo(item_mo, item_update)
+        item_do = self.converter.map_mo_to_do(item_mo)
+        db.merge(item_do)
+        db.commit()
+        return item_mo
+
+    def delete(self, db: Session, id: str) -> Book:
+        item_mo = self.get_by_id(db, id)
+        db.query(BookDO).filter(BookDO.id == id).delete()
+        db.commit()
+        return item_mo
